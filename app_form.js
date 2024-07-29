8a821365a086781581809965c12e705e76356760
princewritecode <princewritecode@gmail.com>
2024-07-29 17:51:40 +0530

    understanding form events updated

--- app_form.js
+++ app_form.js
@@ -1,24 +1,53 @@
-const input_value = document.getElementById('input_name');
-const message_div = document.getElementById('message');
-const para = document.createElement('p');
-const submit_btn = document.getElementById('submit');
-input_value.addEventListener('focus', (e) =>
+const input_value = document.querySelector('input[type="text"]');
+console.log(input_value);
+// const message_div = document.getElementById('message');
+// const para = document.createElement('p');
+// const submit_btn = document.getElementById('submit');
+const output = document.getElementById('output');
+
+// input_value.addEventListener('focus', (e) =>
+// {
+
+//     console.log('focus');
+//     input_value.style.backgroundColor = "rgb(217, 255, 169)";
+//     input_value.style.color = "black";
+
+// });
+// input_value.addEventListener('keydown', (e) =>
+// {
+//     console.log(e.target.value);
+//     para.innerText = `The value entered is: ${e.target.value}`;
+//     message_div.appendChild(para);
+// });
+// submit_btn.addEventListener('click', (e) =>
+// {
+//     e.preventDefault();
+//     para.innerText = ` Value submitted is ${input_value.value}`;
+//     message_div.appendChild(para);
+// });
+
+input_value.addEventListener('input', (e) =>
 {
 
-    console.log('focus');
-    input_value.style.backgroundColor = "rgb(217, 255, 169)";
-    input_value.style.color = "black";
+    output.textContent = `The value entered is
+    ${input_value.value}`;
 
 });
-input_value.addEventListener('keydown', (e) =>
-{
-    console.log(e.target.value);
-    para.innerText = `The value entered is: ${e.target.value}`;
-    message_div.appendChild(para);
-});
-submit_btn.addEventListener('click', (e) =>
+
+document.querySelector('form').addEventListener('submit', (e) =>
 {
     e.preventDefault();
-    para.innerText = ` Value submitted is ${input_value.value}`;
-    message_div.appendChild(para);
+    output.textContent = `A submit event has been fired at
+${e.timeStamp}`;
+});
+
+input_value.addEventListener('focus', (e) =>
+{
+    e.target.style.background = 'lightblue';
+    e.target.style.color = "black";
+});
+
+input_value.addEventListener('copy', () =>
+{
+    alert('input text is copied');
 });
